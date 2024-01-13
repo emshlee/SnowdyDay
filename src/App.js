4517bbcd0147f06713eea74e9ce2a603cf782f9a
Jin Won Lee <89884098+Jin1L@users.noreply.github.com>
2024-01-12 20:06:42 -0500

    stopwatch feature (#1)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Home from './pages/Home.jsx';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
+import Stopwatch from './components/Stopwatch'
 import { useEffect } from 'react';
 import { gapi } from 'gapi-script';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
