2521f2ed4d6544826a20bf1144e7797816c2837a
emshlee <emshlee01@gmail.com>
2024-01-12 20:46:36 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginButton from './components/login';
-import LogoutButton from './components/logout';
 import Stopwatch from './components/Stopwatch'
 import { useEffect } from 'react';
 import { gapi } from 'gapi-script'
-
-const clientId = '314682349715-vvl2mgqregs4hokcnsmkhu8s796ru1da.apps.googleusercontent.com';
+import HomePage from './components/home/homepage';
 
 
 function App() {
 
-  useEffect(() => {
-    function start() { 
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
+  // useEffect(() => {
+  //   function start() { 
+  //     gapi.client.init({
+  //       clientId: clientId,
+  //       scope: ""
+  //     })
+  //   };
 
-    gapi.load('client:auth2', start);
-  });
+  //   gapi.load('client:auth2', start);
+  // });
 
   return (
+
     <div className="App">
-      <LoginButton/>
-      <LogoutButton/>
       <Stopwatch/>
+      <HomePage/>
     </div>
   );
 }
