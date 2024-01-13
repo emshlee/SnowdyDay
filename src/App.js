978944d38e8cc2630cf3be79e506cd25d73031b9
Jinwon Lee <jinwonlee0916@gmail.com>
2024-01-12 22:14:29 -0500

    django setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './img/logo.svg';
 import './App.css';
 import Login from './pages/Login.jsx';
@@ -11,7 +12,13 @@ import { gapi } from 'gapi-script';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from './context/AuthContext.js';
 import { useContext } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Stopwatch from "./components/Stopwatch";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
+import HomePage from "./components/home/homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Stopwatch from './components/Stopwatch'
 import { useEffect } from 'react';
@@ -31,6 +38,15 @@ function App() {
     return children
   }
 
+  const studyClick = () => {
+    // Your button click logic here
+    // console.log('Button clicked!');
+    fetch("/study") // Assuming your Django server is running on the same domain
+      .then((response) => response.json()) // If the response is JSON, you can parse it
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -46,7 +62,12 @@ function App() {
         </Route> */}
       </Routes>
     </BrowserRouter>
-  
+        //   <Router>
+        //   <Routes>
+        //     <Route path="/" Component={HomePage} />
+        //     <Route path="/study" Component={Stopwatch} />
+        //   </Routes>
+        // </Router>
   );
 }
 
