93277dd1a5830f3ccfb44a1a66f751531c8a6601
Ahreum Lee <ahreuml0304@gmail.com>
2024-01-13 16:10:47 -0500

    navbar image fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import React, { useContext } from "react";
 import {signOut} from "firebase/auth";
 import { auth } from '../firebase';
 import { AuthContext } from "../context/AuthContext";
-import Search from "./Search";
 
 const Navbar = () => {
 
     const {currentUser} = useContext(AuthContext);
+
     return (
         <div className="navbar">
             <div className="user">
                 
-                <img src="currentUser.photoURL" alt="" />
+                <img src={currentUser.photoURL} alt="" />
                 <span>{currentUser.displayName}</span>
                 <button onClick={()=> signOut(auth)}>Logout</button>
             </div>
